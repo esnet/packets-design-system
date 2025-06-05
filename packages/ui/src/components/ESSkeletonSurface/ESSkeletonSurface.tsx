f7f38a9d1ab180c170888dea5ef11c18c9ff65cd
Jason Roeckle <jjroeckle@es.net>
2025-06-05 14:27:16 -0700

    feat - Refactored Component - Skeletal Pill become more generically useful Skeletal Surface matching new designs provided by Rive

--- packages/ui/src/components/ESSkeletonSurface/ESSkeletonSurface.tsx
+++ packages/ui/src/components/ESSkeletonSurface/ESSkeletonSurface.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { CSSProperties, FC } from "react";
 
 import { ESSkeletonSurfaceProps } from "./ESSkeletonSurfaceProps";
 
@@ -15,12 +15,23 @@ import styles from "./ESSkeletonSurface.module.css";
  */
 const ESSkeletonSurface: FC<ESSkeletonSurfaceProps> = ({
   isSquare = false,
+  className = "",
+  strokeSize = "var(--esnet-size-border-width-medium, 3px)",
+  strokeRadius = "var(--esnet-size-spacing-large, 2rem)",
+  height = "var(--esnet-size-touchtarget-min, 2rem)",
 }) => {
+  const inlineStyles = {
+    "--stroke-size": strokeSize,
+    "--stroke-radius": strokeRadius,
+    "--height": height,
+  } as CSSProperties;
+
   return (
     <div
       role="alert"
       aria-busy="true"
-      className={`${styles.skeletonSurface} ${isSquare ? "" : styles.rounded}`}
+      className={`${styles.skeletonSurface} ${isSquare ? "" : styles.rounded} ${className}`}
+      style={inlineStyles}
     ></div>
   );
 };
