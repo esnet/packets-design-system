7fa7b149e558031e4bd2a4f914c0eafc018b386c
Ethan Chennault <ethanolchennault@gmail.com>
2025-06-26 13:06:27 -0700

    fix - changed ESIconButtonProps incorrect className type

--- packages/ui/src/components/ESIconButton/ESIconButton.types.ts
+++ packages/ui/src/components/ESIconButton/ESIconButton.types.ts
@@ -1,7 +1,9 @@
+import { ButtonHTMLAttributes, ReactNode } from "react";
+
 export interface ESIconButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  children: React.ReactNode;
-  className?: "string";
+  extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+  className?: string;
   variant: "primary" | "secondary" | "branded" | "tertiary" | "destructive"; // Type of Button
   square?: boolean;
   disabled?: boolean;
