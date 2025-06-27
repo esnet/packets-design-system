25455ac3cab9189bbcf842da81d62d40a3c2403c
Ethan Chennault <37466516+3than0ls@users.noreply.github.com>
2025-06-27 14:29:22 -0700

    docs - added arg values to storyboard

--- apps/docs/src/stories/ESInputSearch.stories.tsx
+++ apps/docs/src/stories/ESInputSearch.stories.tsx
@@ -1,15 +1,54 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { ESInputSearch } from "@esnet/packets-ui";
-import { Brain, X } from "lucide-react";
+import { Brain, SearchIcon, SearchX, X } from "lucide-react";
 
 const meta: Meta<typeof ESInputSearch> = {
   title: "Components/ESInputSearch",
   component: ESInputSearch,
   tags: ["autodocs"],
-  argTypes: {},
-  args: {},
-
+  argTypes: {
+    variant: {
+      control: { type: "radio" },
+      options: ["default", "branded"],
+      defaultValue: "default",
+    },
+    error: {
+      control: { type: "boolean" },
+    },
+    disabled: {
+      control: { type: "boolean" },
+    },
+    onSearchClick: {
+      control: { type: "object" },
+    },
+    onSearchXClick: {
+      control: { type: "object" },
+    },
+    SearchIcon: {
+      control: "object",
+      description:
+        "Lucide icon to represent search, default to Lucide's Search",
+    },
+    SearchXIcon: {
+      control: "object",
+      description:
+        "Lucide icon to represent search, default to Lucide's SearchX",
+    },
+  },
+  args: {
+    error: false,
+    variant: "default",
+    disabled: false,
+    SearchIcon: SearchIcon,
+    SearchXIcon: SearchX,
+  },
   parameters: {
+    docs: {
+      description: {
+        component:
+          "An extension of ESInputText with some preset control buttons",
+      },
+    },
     design: {
       type: "figma",
       url: "https://www.figma.com/design/cPesLecFaiSRJU83KAhhRH/Design-System-Components?node-id=5266-1735&p=f&t=GM7I6Iu2ED85N1w0-0",
