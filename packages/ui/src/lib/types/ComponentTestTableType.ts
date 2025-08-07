2abb23f88c0752955e28c24d5bae376733f15969
Ethan Chennault <ethanolchennault@gmail.com>
2025-08-07 13:38:42 -0700

    test - add ComponentTestTableType for creating table tests

---
+++ packages/ui/src/lib/types/ComponentTestTableType.ts
@@ -0,0 +1,68 @@
+/**
+ * ComponentTestTableType defines the structure of a test table for component with ComponentProps.
+ *
+ * Due to some issue with Playwright's experimental component testing, we can't simply create a utility function that
+ * takes in a component as an argument and tests it under every state, theme, etc.
+ *
+ * Thus, we unfortunately have to define the test table structure here, then copypaste boilerplate to
+ * run these tests in every test file.
+ *
+ * The boilerplate looks like below for component ESComponent
+ */
+// test.describe("ESComponent", () => {
+//   const {
+//     testTable,
+//     themes,
+//     actionStates,
+//   }: ComponentTestTableType<ESComponentProps> = {
+//     testTable: [
+//       { name: "test-1", props: { prop1: "foo" } },
+//       { name: "test-2", props: { prop1: "bar" } },
+//       // etc...
+//     ],
+//     themes: ["light", "dark"],
+//     actionStates: ["hover", "focus", "active"],
+//   };
+//
+//   testTable.forEach(({ name, props }) => {
+//     themes.forEach((theme) => {
+//       let themedComponent = (
+//         <div style={{ padding: "8px" }}>
+//           <ESComponent {...props} />
+//         </div>
+//       );
+//       if (theme === "dark") {
+//         themedComponent = <div className="dark">{themedComponent}</div>;
+//       }
+//       test(`${name}-${theme}`, async ({ mount }) => {
+//         const component = await mount(themedComponent);
+//         await expect(component).toHaveScreenshot();
+//       });
+//       actionStates.forEach((state) => {
+//         test(`${name}-${theme}-${state}`, async ({ page, mount }) => {
+//           // do any locator selection if needed
+//           const component = await mount(themedComponent);
+//           if (state === "focus") await component.focus();
+//           if (state === "hover" || state === "active") await component.hover();
+//           if (state === "active") await page.mouse.down();
+//           await expect(component).toHaveScreenshot();
+//         });
+//       });
+//     });
+//   });
+// });
+
+type TestTableRow<ComponentProps> = {
+  name: string;
+  props: ComponentProps;
+};
+
+type Themes = "light" | "dark";
+
+type ActionStates = "hover" | "focus" | "active";
+
+export interface ComponentTestTableType<ComponentProps> {
+  testTable: TestTableRow<ComponentProps>[];
+  themes: Themes[];
+  actionStates: ActionStates[];
+}
