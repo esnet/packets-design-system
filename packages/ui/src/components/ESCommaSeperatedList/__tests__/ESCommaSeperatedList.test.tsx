15a89cff2b2421659339489c7720cf697a17f106
Ethan Chennault <ethanolchennault@gmail.com>
2025-08-14 12:21:58 -0700

    test - update test and screenshots

--- packages/ui/src/components/ESCommaSeperatedList/__tests__/ESCommaSeperatedList.test.tsx
+++ packages/ui/src/components/ESCommaSeperatedList/__tests__/ESCommaSeperatedList.test.tsx
@@ -1,18 +1,48 @@
+import * as React from "react";
 import { test, expect } from "@playwright/experimental-ct-react";
-import React from "react";
+import { ComponentTestTableType } from "../../../lib/types/ComponentTestTableType";
+import { ComponentTestBox } from "../../../lib/utils/ComponentTestBox";
+import { ESCommaSeperatedListProps } from "../ESCommaSeperatedList.types";
 import ESCommaSeperatedList from "../ESCommaSeperatedList";
-
 test.describe("ESCommaSeperatedList", () => {
-  test("list of items", async ({ mount }) => {
-    const component = await mount(
-      <ESCommaSeperatedList items={["Item 1", "Item 2", "Item 3"]} />
-    );
-    await expect(component).toHaveScreenshot();
-    await expect(component).toHaveText("Item 1Item 2Item 3");
-  });
-
-  test("empty list", async ({ mount }) => {
-    const component = await mount(<ESCommaSeperatedList items={[]} />);
-    await expect(component).toHaveText("");
+  const {
+    testTable,
+    themes,
+  }: ComponentTestTableType<ESCommaSeperatedListProps> = {
+    testTable: [
+      {
+        name: "empty",
+        props: {
+          items: [],
+        },
+      },
+      {
+        name: "three-items",
+        props: {
+          items: ["Item 1", "Item 2", "Item 3"],
+        },
+      },
+    ],
+    themes: ["light", "dark"],
+    actionStates: [],
+  };
+  testTable.forEach(({ name, props }) => {
+    themes.forEach((theme) => {
+      const testBox = (
+        <ComponentTestBox
+          component={
+            <span>
+              This test uses ESCommaSeperatedList inside of a span tag:{" "}
+              <ESCommaSeperatedList {...props} />
+            </span>
+          }
+          theme={theme}
+        />
+      );
+      test(`${name}-${theme}`, async ({ mount }) => {
+        const component = await mount(testBox);
+        await expect(component).toHaveScreenshot();
+      });
+    });
   });
 });
