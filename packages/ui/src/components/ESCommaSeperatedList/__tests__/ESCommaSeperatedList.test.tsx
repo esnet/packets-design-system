2a1f42a1c3ca889e04709c5ffce3b2c0e937801c
Ethan Chennault <ethanolchennault@gmail.com>
2025-08-13 17:18:53 -0700

    test - ESCommaSeperatedList visual regression tests

--- packages/ui/src/components/ESCommaSeperatedList/__tests__/ESCommaSeperatedList.test.tsx
+++ packages/ui/src/components/ESCommaSeperatedList/__tests__/ESCommaSeperatedList.test.tsx
@@ -1,23 +1,18 @@
-// import React from "react";
-// import { render } from "@testing-library/react";
-// import ESCommaSeperatedList from "../ESCommaSeperatedList";
+import { test, expect } from "@playwright/experimental-ct-react";
+import React from "react";
+import ESCommaSeperatedList from "../ESCommaSeperatedList";
 
-// test("It renders without crashing", () => {
-//   const component = render(
-//     <ESCommaSeperatedList
-//       items={["apples", "oranges", "lemons"]}
-//       renderItem={(item) => <p>{item}</p>}
-//     />,
-//   );
-//   expect(component).toBeTruthy();
-// });
+test.describe("ESCommaSeperatedList", () => {
+  test("list of items", async ({ mount }) => {
+    const component = await mount(
+      <ESCommaSeperatedList items={["Item 1", "Item 2", "Item 3"]} />
+    );
+    await expect(component).toHaveScreenshot();
+    await expect(component).toHaveText("Item 1Item 2Item 3");
+  });
 
-// it("Comma Seperated List DOM Snapshot", () => {
-//   const domTree = render(
-//     <ESCommaSeperatedList
-//       items={["apples", "oranges", "lemons"]}
-//       renderItem={(item) => <p>{item}</p>}
-//     />,
-//   );
-//   expect(domTree).toMatchSnapshot();
-// });
+  test("empty list", async ({ mount }) => {
+    const component = await mount(<ESCommaSeperatedList items={[]} />);
+    await expect(component).toHaveText("");
+  });
+});
