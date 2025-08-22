10934cceb57d93ce33b9ea8f2a1b0b80522944fd
Ethan Chennault <ethanolchennault@gmail.com>
2025-08-22 15:02:34 -0700

    test - ESIconButton visual regression tests

--- packages/ui/src/components/ESIconButton/__tests__/ESIconButton.test.tsx
+++ packages/ui/src/components/ESIconButton/__tests__/ESIconButton.test.tsx
@@ -1,58 +1,41 @@
-// import React from "react";
-// import { render } from "@testing-library/react";
-// import ESIconButton from "../ESIconButton";
-// import { SettingsIcon } from "lucide-react";
-
-// test("It renders without crashing", () => {
-//   const component = render(
-//     <ESIconButton variant={"primary"}>
-//       <SettingsIcon />
-//     </ESIconButton>,
-//   );
-//   expect(component).toBeTruthy();
-// });
-
-// it("Primary matches DOM Snapshot", () => {
-//   const domTree = render(
-//     <ESIconButton variant={"primary"}>
-//       <SettingsIcon />
-//     </ESIconButton>,
-//   );
-//   expect(domTree).toMatchSnapshot();
-// });
-
-// it("Secondary matches DOM Snapshot", () => {
-//   const domTree = render(
-//     <ESIconButton variant={"secondary"}>
-//       <SettingsIcon />
-//     </ESIconButton>,
-//   );
-//   expect(domTree).toMatchSnapshot();
-// });
-
-// it("Teritary matches DOM Snapshot", () => {
-//   const domTree = render(
-//     <ESIconButton variant={"tertiary"}>
-//       <SettingsIcon />
-//     </ESIconButton>,
-//   );
-//   expect(domTree).toMatchSnapshot();
-// });
-
-// it("Destructive matches DOM Snapshot", () => {
-//   const domTree = render(
-//     <ESIconButton variant={"destructive"}>
-//       <SettingsIcon />
-//     </ESIconButton>,
-//   );
-//   expect(domTree).toMatchSnapshot();
-// });
-
-// it("Branded matches DOM Snapshot", () => {
-//   const domTree = render(
-//     <ESIconButton variant={"branded"}>
-//       <SettingsIcon />
-//     </ESIconButton>,
-//   );
-//   expect(domTree).toMatchSnapshot();
-// });
+import * as React from "react";
+import { test, expect } from "@playwright/experimental-ct-react";
+import { ComponentTestTableType } from "../../../lib/types/ComponentTestTableType";
+import { ComponentTestBox } from "../../../lib/utils/ComponentTestBox";
+import { ESIconButtonProps } from "../ESIconButton.types";
+import ESIconButton from "../ESIconButton";
+test.describe("ESIconButton", () => {
+  const { testTable, themes }: ComponentTestTableType<ESIconButtonProps> = {
+    testTable: [
+      // since ESIconButton is built on top of ESButton, only test the parts specific to ESButton
+      {
+        name: "default",
+        props: {
+          name: "settings",
+        },
+      },
+      {
+        name: "square",
+        props: {
+          name: "settings",
+        },
+      },
+    ],
+    themes: ["light", "dark"],
+    actionStates: [],
+  };
+  testTable.forEach(({ name, props }) => {
+    themes.forEach((theme) => {
+      const testBox = (
+        <ComponentTestBox
+          component={<ESIconButton {...props} />}
+          theme={theme}
+        />
+      );
+      test(`${name}-${theme}`, async ({ mount }) => {
+        const component = await mount(testBox);
+        await expect(component).toHaveScreenshot();
+      });
+    });
+  });
+});
