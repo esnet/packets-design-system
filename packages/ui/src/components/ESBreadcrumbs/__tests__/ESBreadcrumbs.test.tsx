70c0f6f8706c4a63a8a32496aec032547c08e6ef
Ethan Chennault <ethanolchennault@gmail.com>
2025-09-16 12:13:28 -0700

    test - add test file for ESBreadcrumbs

--- packages/ui/src/components/ESBreadcrumbs/__tests__/ESBreadcrumbs.test.tsx
+++ packages/ui/src/components/ESBreadcrumbs/__tests__/ESBreadcrumbs.test.tsx
@@ -1,41 +1,57 @@
-// import React from "react";
-// import { render } from "@testing-library/react";
-// import ESBreadcrumbs from "../ESBreadcrumbs";
-
-// test("It renders without crashing", () => {
-//   const component = render(
-//     <ESBreadcrumbs
-//       breadcrumbs={[
-//         {
-//           href: "http://www.es.net",
-//           target: "_blank",
-//           children: "www.es.net",
-//         },
-//         {
-//           href: "/?path=/docs/getting-started--docs",
-//           children: "Getting Started",
-//         },
-//       ]}
-//     />,
-//   );
-//   expect(component).toBeTruthy();
-// });
-
-// it("It renders a DOM Snapshot", () => {
-//   const domTree = render(
-//     <ESBreadcrumbs
-//       breadcrumbs={[
-//         {
-//           href: "http://www.es.net",
-//           target: "_blank",
-//           children: "www.es.net",
-//         },
-//         {
-//           href: "/?path=/docs/getting-started--docs",
-//           children: "Getting Started",
-//         },
-//       ]}
-//     />,
-//   );
-//   expect(domTree).toMatchSnapshot();
-// });
+import * as React from "react";
+import { test, expect } from "@playwright/experimental-ct-react";
+import { ComponentTestTableType } from "../../../lib/types/ComponentTestTableType";
+import { ComponentTestBox } from "../../../lib/utils/ComponentTestBox";
+import { ESBreadcrumbsProps } from "../ESBreadcrumbs.types";
+import ESBreadcrumbs from "../ESBreadcrumbs";
+import { defaultRenderLink } from "../../../lib/utils/link";
+test.describe("ESBreadcrumbs", () => {
+  const breadcrumbs = [
+    {
+      href: "/",
+      children: "Home",
+    },
+    {
+      href: "/circuits",
+      children: "Circuits",
+    },
+    {
+      href: "/circuits/THX-1138",
+      children: "THX-1138",
+    },
+  ];
+  const { testTable, themes }: ComponentTestTableType<ESBreadcrumbsProps> = {
+    testTable: [
+      {
+        name: "default",
+        props: {
+          breadcrumbs,
+        },
+      },
+      {
+        name: "different-rendering",
+        props: {
+          breadcrumbs: breadcrumbs,
+          renderLink: (linkProps) => linkProps.children,
+          // renderLink: (linkProps) => <a {...linkProps}>{linkProps.children}</a>,
+        },
+      },
+    ],
+    themes: ["light", "dark"],
+    actionStates: [],
+  };
+  testTable.forEach(({ name, props }) => {
+    themes.forEach((theme) => {
+      const testBox = (
+        <ComponentTestBox
+          component={<ESBreadcrumbs {...props} />}
+          theme={theme}
+        />
+      );
+      test(`${name}-${theme}`, async ({ mount }) => {
+        const component = await mount(testBox);
+        await expect(component).toHaveScreenshot();
+      });
+    });
+  });
+});
