2ddc2016f40f5800c475d29b3913f7fa47f40dd1
Ethan Chennault <37466516+3than0ls@users.noreply.github.com>
2025-09-18 15:44:51 -0700

    feat - filter non chip children out

--- packages/ui/src/components/ESChipGroup/ESChipGroup.tsx
+++ packages/ui/src/components/ESChipGroup/ESChipGroup.tsx
@@ -2,17 +2,26 @@ import * as React from "react";
 import clsx from "clsx";
 import styles from "./ESChipGroup.module.css";
 import { ESChipGroupProps } from "./ESChipGroup.types";
+import ESChip from "../ESChip/ESChip";
 
 /**
  * ESChipGroup Component
  *
- * Children should be ESChip components.
+ * Composable component that is a container of ESChip components.
+ * If a child is not a chip component, it is not rendered.
  *
  * @param {ESChipGroupProps} props
- * @returns {React.ReactElement}
+ * @returns {React.ReactNode}
  */
 const ESChipGroup: React.FC<ESChipGroupProps> = ({ className, children }) => {
-  return <div className={clsx(styles.ESChipGroup, className)}>{children}</div>;
+  const chipChildren = React.useMemo(() => {
+    return React.Children.toArray(children).filter(
+      (child) => (child as React.ReactElement).type !== ESChip.displayName
+    );
+  }, [children]);
+  return (
+    <div className={clsx(styles.ESChipGroup, className)}>{chipChildren}</div>
+  );
 };
 
 ESChipGroup.displayName = "ESChipGroup";
