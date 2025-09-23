5abfa8cda2cb16116d7d6f65e950f278aed2cdbd
Ethan Chennault <37466516+3than0ls@users.noreply.github.com>
2025-09-23 10:16:37 -0700

    test - update typeahead tests

--- packages/ui/src/components/ESInputTypeahead/__tests__/ESInputTypeahead.test.tsx
+++ packages/ui/src/components/ESInputTypeahead/__tests__/ESInputTypeahead.test.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { test, expect } from "@playwright/experimental-ct-react";
 import { ComponentTestTableType } from "../../../lib/types/ComponentTestTableType";
-import { ComponentTestBox } from "../../../lib/utils/ComponentTestBox";
 import ESInputTypeahead from "../ESInputTypeahead";
 import { ESInputTypeaheadProps } from "../ESInputTypeahead.types";
 
@@ -32,92 +31,92 @@ const options = [
 ];
 
 test.describe("ESInputTypeahead", () => {
-  const { testTable, themes }: ComponentTestTableType<ESInputTypeaheadProps> = {
-    testTable: [
-      {
-        name: "default",
-        props: {
-          options: options,
-        },
-      },
-      //   {
-      //     name: "empty",
-      //     props: {
-      //       options: [],
-      //     },
-      //   },
-      //   {
-      //     name: "loading",
-      //     props: {
-      //       options,
-      //       loading: true,
-      //     },
-      //   },
-    ],
-    themes: ["light", "dark"],
-    actionStates: [],
-  };
-  testTable.forEach(({ name, props }) => {
-    themes.forEach((theme) => {
-      test(`${name}-${theme}`, async ({ mount }) => {
-        test.setTimeout(120_000);
-        const testBox = (
-          <div
-            style={{
-              position: "relative",
-              padding: "8px",
-              height: "340px",
-              width: "900px",
-            }}
-            className={theme}
-          >
-            <ESInputTypeahead {...props} />
-          </div>
-        );
-        const component = await mount(testBox);
-        const input = component.getByRole("textbox");
-        await expect(input).toBeVisible();
-        await input.fill("m");
-        await input.click();
-        await expect(
-          component.getByRole("listbox", { name: "Typeahead Dropdown Options" })
-        ).toBeVisible();
-        await component
-          .getByRole("button", { name: "Lorem ipsum dolor sit amet" })
-          .click();
-        await component
-          .getByRole("button", { name: "Consectetur adipiscing elit," })
-          .focus();
-        await component
-          .getByRole("button", { name: "Ut enim ad minim veniam" })
-          .hover();
-        await expect(component).toHaveScreenshot();
-      });
+  ["light", "dark"].forEach((theme) => {
+    test(`default-${theme}`, async ({ mount }) => {
+      const testBox = (
+        <div
+          style={{
+            padding: "8px",
+            height: "340px",
+            width: "900px",
+          }}
+          className={theme}
+        >
+          <ESInputTypeahead options={options} />
+        </div>
+      );
+      const component = await mount(testBox);
+      const input = component.getByRole("textbox");
+      await expect(input).toBeVisible();
+      await input.fill("m");
+      await input.click();
+      await expect(
+        component.getByRole("listbox", {
+          name: "Typeahead Dropdown Options",
+        })
+      ).toBeVisible();
+      await component
+        .getByRole("button", { name: "Lorem ipsum dolor sit amet" })
+        .click();
+      await component
+        .getByRole("button", { name: "Consectetur adipiscing elit," })
+        .focus();
+      await component
+        .getByRole("button", { name: "Ut enim ad minim veniam" })
+        .hover();
+      await expect(component).toHaveScreenshot();
+    });
 
-      // additionally, test
-      if (name !== "default") return;
-      test(`actions-${theme}`, async ({ mount }) => {
-        const testBox = (
-          <div
-            style={{
-              position: "relative",
-              padding: "8px",
-              height: "340px",
-              width: "900px",
-            }}
-            className={theme}
-          >
-            <ESInputTypeahead {...props} />
-          </div>
-        );
-        const component = await mount(testBox);
-        const input = component.nth(2);
-        await input.getByRole("button").nth(6).click();
-        await input.getByRole("button").nth(7).click();
-        await input.hover();
-        await input.getByRole("button").nth(5).focus();
-        await expect(component).toHaveScreenshot();
-      });
+    test(`loading-${theme}`, async ({ mount }) => {
+      const testBox = (
+        <div
+          style={{
+            padding: "8px",
+            height: "100px",
+            width: "300px",
+          }}
+          className={theme}
+        >
+          <ESInputTypeahead options={options} loading />
+        </div>
+      );
+      const component = await mount(testBox);
+      const input = component.getByRole("textbox");
+      await expect(input).toBeVisible();
+      await input.click();
+      await input.fill("loading value");
+      await expect(
+        component.getByRole("listbox", {
+          name: "Typeahead Dropdown Options",
+        })
+      ).toBeVisible();
+      await expect(component).toHaveScreenshot();
+    });
+
+    test(`empty-${theme}`, async ({ mount }) => {
+      const testBox = (
+        <div
+          style={{
+            padding: "8px",
+            height: "100px",
+            width: "300px",
+          }}
+          className={theme}
+        >
+          <ESInputTypeahead options={[]} />
+        </div>
+      );
+      const component = await mount(testBox);
+      const input = component.getByRole("textbox");
+      await expect(input).toBeVisible();
+      await input.click();
+      await input.fill("empty");
+      await expect(
+        component.getByRole("listbox", {
+          name: "Typeahead Dropdown Options",
+        })
+      ).toBeVisible();
+      await expect(component).toHaveScreenshot();
     });
   });
 });
