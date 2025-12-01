b89111e5cef3c1bc87df0188177e9e170b305537
Ethan Chennault <37466516+3than0ls@users.noreply.github.com>
2025-12-01 12:20:03 -0800

    refactor - fix typo carat to caret

--- packages/ui/src/components/ESDropdown/ESDropdown.types.ts
+++ packages/ui/src/components/ESDropdown/ESDropdown.types.ts
@@ -1,8 +1,8 @@
 import { ComponentPropsWithoutRef, ReactNode } from "react";
 
 export interface ESDropdownProps extends ComponentPropsWithoutRef<"div"> {
-  /** Whether to include a carat for the dropdown menu pointing towards the anchor. */
-  carat?: boolean;
+  /** Whether to include a caret for the dropdown menu pointing towards the anchor. */
+  caret?: boolean;
   /** Dropdown mode (whether to show contents on hover, active, or both) */
   mode?: "hover" | "active" | "both";
 }
