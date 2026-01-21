f4d30fcbdb8ba9b72c39a921bf865a2bb773d776
Ethan Chennault <37466516+3than0ls@users.noreply.github.com>
2026-01-21 10:58:50 -0800

    test - remove all jest tests in favor of playwright tests

--- packages/ui/playwright.config.ts
+++ packages/ui/playwright.config.ts
@@ -9,7 +9,7 @@ import { defineConfig, devices } from "@playwright/experimental-ct-react";
  */
 export default defineConfig({
   testDir: "./",
-  testMatch: "**/__tests__/*.playwright.test.ts?(x)",
+  testMatch: "**/__tests__/*.test.ts?(x)",
   snapshotPathTemplate:
     "{testFileDir}/__screenshots__/{/projectName}/{arg}{ext}",
   /* Maximum time one test can run for. */
@@ -21,7 +21,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : "50%",
+  workers: process.env.CI ? 1 : 13,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
