b50f6d2226ac204dd435ec51c898b9b31946185f
Ethan Chennault <37466516+3than0ls@users.noreply.github.com>
2026-02-11 12:42:44 -0800

    test - update tests for new ESInputSelect

--- packages/ui/src/components/ESInputSelect/__tests__/ESInputSelect.test.tsx
+++ packages/ui/src/components/ESInputSelect/__tests__/ESInputSelect.test.tsx
@@ -1,36 +1,26 @@
 import * as React from "react";
 import { test, expect } from "@playwright/experimental-ct-react";
 import { ComponentTestTableType } from "../../../lib/types/ComponentTestTableType";
-import { ComponentTestBox } from "../../../lib/utils/ComponentTestBox";
 import { ESInputSelectProps } from "../ESInputSelect.types";
 import ESInputSelect from "../ESInputSelect";
+import ESInputOption from "../../ESInputOption";
+
+const options = [
+  "foo (test)",
+  "bar (test)",
+  "baz (test)",
+  "fizz",
+  "buzz",
+  "foo bar baz fizz buzz and any other extremely long text that may demonstrate text overflow",
+].map((value) => <ESInputOption key={value}>{value}</ESInputOption>);
+
 test.describe("ESInputSelect", () => {
-  const {
-    testTable,
-    themes,
-    actionStates,
-  }: ComponentTestTableType<ESInputSelectProps> = {
+  const { testTable, themes }: ComponentTestTableType<ESInputSelectProps> = {
     testTable: [
       {
         name: "default",
         props: {
-          options: ["foo", "bar", "baz"],
-        },
-      },
-      {
-        name: "options",
-        props: {
-          options: [
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-            "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-            "Ut enim ad minim veniam",
-            "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
-            "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
-            "Excepteur sint occaecat cupidatat non proident",
-            "sunt in culpa qui officia deserunt mollit anim id est laborum",
-          ],
-          error: true,
-          defaultValue: "Default Value",
+          children: options,
         },
       },
     ],
@@ -43,18 +33,18 @@ test.describe("ESInputSelect", () => {
         <div
           style={{
             padding: "8px",
-            height: "340px",
-            width: "900px",
+            height: "300px",
+            width: "500px",
           }}
           className={theme}
         >
-          <ESInputSelect options={props.options} />
+          <ESInputSelect {...props} />
         </div>
       );
       test(`${name}-${theme}`, async ({ mount }) => {
         const component = await mount(testBox);
 
-        const input = component.getByRole("textbox");
+        const input = component.getByRole("combobox");
         await input.click();
         await component.getByRole("option").nth(2).click();
         await input.click();
