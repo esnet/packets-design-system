c04fb0f039522ab42c47f9712f6310af6078cd5b
Ethan Chennault <ethanolchennault@gmail.com>
2026-03-31 12:14:46 -0700

    Remove old packages/ui folder with unused code

--- packages/ui/src/components/ESCheckableList/ESCheckableList.types.ts
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-
-export interface ESCheckableListProps
-  extends React.ComponentPropsWithRef<"fieldset"> {
-  className?: string;
-  legend?: React.ReactNode;
-  children: React.ReactNode;
-}
