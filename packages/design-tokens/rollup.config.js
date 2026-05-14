16a5f0bc556b4c22ae09b7d30d3054c843052a92
Freddy Mande <16734663+FreddKM@users.noreply.github.com>
2026-05-14 13:58:32 -0700

    fix: remove Node 22 incompatible import assertion in tokens rollup config

--- packages/design-tokens/rollup.config.js
+++ packages/design-tokens/rollup.config.js
@@ -1,6 +1,5 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import pkg from "./package.json" assert { type: "json" };
 
 export default [
   // browser-friendly UMD build
